a75acdbbdfd7101dc42c72f39e177aa01dc05778
Gilberto Conde <gilberto.conde@lightshift.xyz>
2024-12-20 17:39:12 +0000

    add list buckets to wasm

--- akave-wasm-sdk/src/lib.rs
+++ akave-wasm-sdk/src/lib.rs
@@ -1,12 +1,13 @@
-mod client;
 mod utils;
 
-// ipcnodeapi_client::IPCNodeAPIClient, IPCNodeAPIRequest
-
-use client::proto::{ipc_node_api_client::IpcNodeApiClient, IpcBucketListRequest};
+use ipc_node_api::{ipc_node_api_client::IpcNodeApiClient, IpcBucketListRequest};
 use tonic_web_wasm_client::Client;
 
 use wasm_bindgen::prelude::*;
+
+pub mod ipc_node_api {
+    tonic::include_proto!("ipcnodeapi");
+}
 
 fn build_client() -> IpcNodeApiClient<Client> {
     let base_url = "http://connect.akave.ai:5500".to_string();
@@ -18,6 +19,9 @@ fn build_client() -> IpcNodeApiClient<Client> {
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
@@ -29,9 +33,19 @@ pub fn greet(name: &str) {
 
 #[wasm_bindgen]
 pub async fn list_buckets(address: &str) -> Result<JsValue, serde_wasm_bindgen::Error> {
+    log("TEST LOG!!!");
     let mut client = build_client();
     let response = client.bucket_list(IpcBucketListRequest {
         address: address.to_owned(),
     });
-    serde_wasm_bindgen::to_value(&response.await.unwrap().into_inner())
+    match response.await {
+        Ok(buckets_resp) => {
+            let buckets_list = buckets_resp.into_inner();
+            log(&buckets_list.clone().buckets.first().unwrap().name);
+            return serde_wasm_bindgen::to_value(&buckets_list);
+        }
+        Err(status) => {
+            return serde_wasm_bindgen::to_value(&status.message());
+        }
+    }
 }
