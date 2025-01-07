5665a070f99dcaeef27c8b97379e3a1d09a8bf43
Gilberto Conde <gilberto.conde@lightshift.xyz>
2025-01-07 13:11:39 +0000

    fix TS types

--- packages/akave-wasm-sdk/build.rs
+++ packages/akave-wasm-sdk/build.rs
@@ -5,10 +5,14 @@ fn main() -> io::Result<()> {
     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
     let descriptor_file = out.join("descriptors.bin");
     let conf = tonic_build::configure()
-        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .extern_path(".google.protobuf.Any", "::prost_wkt_types::Any")
-        .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
-        .extern_path(".google.protobuf.Value", "::prost_wkt_types::Value")
+        .type_attribute(
+            ".",
+            "#[derive(tsify_next::Tsify, serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(".", "#[serde(rename_all = \"camelCase\")]")
+        .type_attribute(".", "#[tsify(into_wasm_abi, from_wasm_abi)]")
+        .field_attribute("created_at", "#[tsify(type = \"String\")]")
+        .extern_path(".google.protobuf", "::prost_wkt_types")
         .file_descriptor_set_path(&descriptor_file)
         .compile_well_known_types(true)
         .build_server(false)
--- packages/frontend-akave-wasm-sdk/src/components/WasmTest.tsx
+++ packages/frontend-akave-wasm-sdk/src/components/WasmTest.tsx
@@ -6,11 +6,54 @@ const WasmTest = () => {
   const { address } = useAccount();
   const handleOnWasmGreetClick = async () => {
     await init();
-    const akave_wasm_sdk = await AkaveWebSDK.new();
-    const result = await akave_wasm_sdk.list_buckets(address as string);
-    console.log(result);
-  };
+    const akaveWasmSdk = await AkaveWebSDK.new();
+    const bucketsResult = await akaveWasmSdk.list_buckets(address as string);
+    console.log("list_buckets:");
+    console.log(bucketsResult.buckets.map((b) => b.name));
+    const bucketsInfoResult = bucketsResult.buckets.map((bInfoRes) =>
+      akaveWasmSdk.view_bucket(address as string, bInfoRes.name),
+    );
+    console.log("view_bucket: (in loop)");
+    console.log((await Promise.all(bucketsInfoResult)).map((info) => info));
 
+    console.log("list_files: (in loop / bucket)");
+    const files = (
+      await Promise.all(
+        bucketsResult.buckets.map((b) =>
+          akaveWasmSdk.list_files(address as string, b.name),
+        ),
+      )
+    ).map((f) => f.list);
+    console.log(
+      files
+        .map(
+          (fb, i) =>
+            `${bucketsResult.buckets[i].name}:\n${fb.map((f) => f.rootCid).reduce((prev, curr) => prev + "\n" + curr, "")}`,
+        )
+        .reduce((prev, curr) => prev + "\n" + curr, ""),
+    );
+    console.log("view_file_info: (in loop)");
+    const fileList = files.map((fs, i) =>
+      fs
+        .filter((f) => Boolean(f.name))
+        .map((f) => {
+          console.log({ name: f.name });
+          return akaveWasmSdk.view_file_info(
+            address as string,
+            f.name,
+            bucketsResult.buckets[i].name,
+          );
+        }),
+    );
+    console.log(
+      fileList
+        .map(async (fb, i) => {
+          const nfb = await Promise.all(fb);
+          return `${bucketsResult.buckets[i].name}:\n${nfb.map((f) => f).reduce((prev, curr) => prev + "\n" + curr, "")}`;
+        })
+        .reduce((prev, curr) => prev + "\n" + curr, ""),
+    );
+  };
   return <button onClick={handleOnWasmGreetClick}>Run greet</button>;
 };
 
