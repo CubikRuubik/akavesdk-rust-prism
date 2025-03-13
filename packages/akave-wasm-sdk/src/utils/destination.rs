d36ca9b222d66be5c7048c3218113b90fa42e07a
Essam Hassan <es.hassan187@gmail.com>
2025-03-13 13:48:13 +0400

    fix destination api

--- packages/akave-wasm-sdk/src/utils/destination.rs
+++ packages/akave-wasm-sdk/src/utils/destination.rs
@@ -46,12 +46,14 @@ impl Destination {
     }
 
     #[cfg(target_arch = "wasm32")]
-    pub fn new(destination_path: &str, file_name: &str) -> Self {
+    pub fn new(destination_path: &str, file_name: &str) -> Result<Self, Box<dyn std::error::Error>> {
         let full_path = format!("{}{}", destination_path, file_name);
-        Destination {
+        Ok(
+            Destination {
             buffer: Vec::new(),
             path: full_path,
-        }
+            }
+        )
     }
 
     pub fn write(&mut self, data: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
@@ -74,7 +76,7 @@ impl Destination {
     }
 
     #[cfg(target_arch = "wasm32")]
-    pub fn flush(&mut self) -> Result<(), JsValue> {
+    pub fn flush(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         Ok(())
     }
 
@@ -84,31 +86,29 @@ impl Destination {
         Ok(())
     }
 
+    // TODO: clean unwraps, switch to std errors
     #[cfg(target_arch = "wasm32")]
-    pub fn finalize(&mut self) -> Result<(), JsValue> {
+    pub fn finalize(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         // Convert Vec<u8> to a Uint8Array
         let uint8_array = Uint8Array::new_with_length(self.buffer.len() as u32);
         uint8_array.copy_from(&self.buffer);
 
         // Create a Blob from the Uint8Array
-        let blob = Blob::new_with_u8_array_sequence(&JsValue::from(&uint8_array))?;
-
-        // Create a Blob from the Uint8Array
-        let blob = Blob::new_with_u8_array_sequence(&JsValue::from(&uint8_array))?;
-        let url = Url::create_object_url_with_blob(&blob)?;
+        let blob = Blob::new_with_u8_array_sequence(&JsValue::from(&uint8_array)).unwrap();
+        let url = Url::create_object_url_with_blob(&blob).unwrap();
 
         // Optionally, you can create a download link to trigger the browser's download dialog
         let link = web_sys::window()
             .unwrap()
             .document()
             .unwrap()
-            .create_element("a")?;
-        link.set_attribute("href", &url)?;
-        link.set_attribute("download", &self.path)?;
-        link.dyn_into::<web_sys::HtmlElement>()?.click();
+            .create_element("a").unwrap();
+        link.set_attribute("href", &url).unwrap();
+        link.set_attribute("download", &self.path).unwrap();
+        link.dyn_into::<web_sys::HtmlElement>().unwrap().click();
 
         // Revoke the URL after the download
-        Url::revoke_object_url(&url)?;
+        Url::revoke_object_url(&url).unwrap();
 
         Ok(())
     }
