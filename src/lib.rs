1f4e86dd06fa0d3835a2bea262110f30960648a3
Krzysztof Woś <krzysztof.wos@lightshift.xyz>
2025-06-04 10:09:48 +0900

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 mod allocator;
 mod blockchain;
+pub mod logger;
 pub mod sdk;
-mod utils;
 mod sdk_types;
 mod types;
-pub mod logger;
+mod utils;
 
 #[cfg(target_arch = "wasm32")]
 pub mod wasm;
@@ -12,11 +12,10 @@ pub mod wasm;
 #[cfg(target_arch = "wasm32")]
 mod panic_handler;
 
-
 #[cfg(not(target_arch = "wasm32"))]
 fn get_nonce() -> web3::types::U256 {
     use std::time::{SystemTime, UNIX_EPOCH};
-    
+
     let timestamp = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards")
@@ -27,16 +26,15 @@ fn get_nonce() -> web3::types::U256 {
 #[cfg(target_arch = "wasm32")]
 fn get_nonce() -> web3::types::U256 {
     use wasm_bindgen::prelude::*;
-    
+
     #[wasm_bindgen]
     extern "C" {
         #[wasm_bindgen(js_namespace = Date)]
         fn now() -> f64;
     }
-    
+
     // Get timestamp in milliseconds from JavaScript's Date.now()
     // and convert to microseconds
     let timestamp = (now() * 1000.0) as u128;
     web3::types::U256::from(timestamp)
 }
-
